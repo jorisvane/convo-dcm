fd842283bbe1db6cf69296eb5f38bb4fe634e822
jorisvane <70818648+jorisvane@users.noreply.github.com>
2021-06-03 17:32:32 +0200

    test job change

--- job_test.sh
+++ job_test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SBATCH --partition=general
 #SBATCH --qos=short
-#SBATCH --time=4:00:00
+#SBATCH --time=1:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=12000
 #SBATCH --gres=gpu:1
 
@@ -15,9 +15,4 @@ module load miniconda/3.8
 module load cuda/11.1
 module load cudnn/11.1-8.0.5.39
 
-module load matlab/R2019b
-module load cuda/10.0 cudnn/10.0-7.4.2.24
-
-srun matlab < CNDCM_v2.m
-
-srun miniconda < train_cluster.py
+srun miniconda < cluster_test.py
