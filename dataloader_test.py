06906e0fb29860452e97f026353aa85d717b0018
jorisvane <70818648+jorisvane@users.noreply.github.com>
2021-06-08 12:37:54 +0200

    Update dataloader_test.py

--- dataloader_test.py
+++ dataloader_test.py
@@ -1,8 +1,8 @@
 import torch
+import torchvision
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
-import torchvision
 from torch.utils.data import DataLoader
 
 from CustomDataset import ImageChoiceDataset
