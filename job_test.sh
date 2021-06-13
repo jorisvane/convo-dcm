cf76971869738e032f9409f9187f332495e57b95
jorisvane <70818648+jorisvane@users.noreply.github.com>
2021-06-13 14:47:36 +0200

    Update job_test.sh

--- job_test.sh
+++ job_test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH --partition=general
-#SBATCH --qos=short
-#SBATCH --time=1:00:00
+#SBATCH --qos=long
+#SBATCH --time=4:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=12000
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:turing:1
 
 
 
@@ -14,4 +14,4 @@ module use /opt/insy/modulefiles
 module load cuda/11.1
 module load cudnn/11.1-8.0.5.39
 
-python3 dataloader_test.py $*
+python3 train_eval_ResNet50_cluster.py $*
