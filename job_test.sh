17be4039de7011c7978bfe7cad0c50a03bf26de5
jorisvane <70818648+jorisvane@users.noreply.github.com>
2021-06-16 12:10:30 +0200

    Testing GPU efficiency

--- job_test.sh
+++ job_test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH --partition=general
-#SBATCH --qos=short
-#SBATCH --time=1:00:00
+#SBATCH --qos=long
+#SBATCH --time=4:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8000
-#SBATCH --gres=gpu:1
+#SBATCH --mem=5000
+#SBATCH --gres=gpu:turing:1
 
 
 
@@ -14,4 +14,6 @@ module use /opt/insy/modulefiles
 module load cuda/11.1
 module load cudnn/11.1-8.0.5.39
 
-srun python3 test1.py $*
+previous=$(nvidia-smi --query-accounted-apps=gpu_utilization,mem_utilization,max_memory_usage,time --format=csv | tail -n +2)
+srun python3 train_eval_ResNet50_cluster.py $*
+nvidia-smi --query-accounted-apps=gpu_utilization,mem_utilization,max_memory_usage,time --format=csv | grep -v -F "$previous"
