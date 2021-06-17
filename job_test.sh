c726509c77a9a720fbe8fed4bdfc27e8175f9c5c
jorisvane <70818648+jorisvane@users.noreply.github.com>
2021-06-17 16:50:50 +0200

    Update job_test.sh

--- job_test.sh
+++ job_test.sh
@@ -3,9 +3,9 @@
 #SBATCH --qos=long
 #SBATCH --time=4:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=5000
-#SBATCH --gres=gpu:pascal:1
+#SBATCH --gres=gpu:turing:1
 
 
 
