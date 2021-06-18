7c1138bc0346b16355cbe62d20128fe5d7f6c7b7
jorisvane <70818648+jorisvane@users.noreply.github.com>
2021-06-18 18:13:13 +0200

    Update job_test.sh

--- job_test.sh
+++ job_test.sh
@@ -15,5 +15,5 @@ module load cuda/11.1
 module load cudnn/11.1-8.0.5.39
 
 previous=$(nvidia-smi --query-accounted-apps=gpu_utilization,mem_utilization,max_memory_usage,time --format=csv | tail -n +2)
-srun python3 resnet_only_loss.py $*
+srun python3 train_eval_ResNet50_cluster.py $*
 nvidia-smi --query-accounted-apps=gpu_utilization,mem_utilization,max_memory_usage,time --format=csv | grep -v -F "$previous"
