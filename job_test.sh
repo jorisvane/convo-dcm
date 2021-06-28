0adc2e8434fedcdda0566cdcbd29287f68caf36f
jorisvane <70818648+jorisvane@users.noreply.github.com>
2021-06-28 21:38:13 +0200

    Update job_test.sh

--- job_test.sh
+++ job_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --partition=general
-#SBATCH --qos=long
-#SBATCH --time=6:00:00
+#SBATCH --qos=short
+#SBATCH --time=3:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=5000
@@ -15,5 +15,5 @@ module load cuda/11.1
 module load cudnn/11.1-8.0.5.39
 
 previous=$(nvidia-smi --query-accounted-apps=gpu_utilization,mem_utilization,max_memory_usage,time --format=csv | tail -n +2)
-srun python3 Test_early_stopping.py $*
+srun python3 full_train.py $*
 nvidia-smi --query-accounted-apps=gpu_utilization,mem_utilization,max_memory_usage,time --format=csv | grep -v -F "$previous"
